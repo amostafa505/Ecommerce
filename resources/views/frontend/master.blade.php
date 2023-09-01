a99d6c653d30dac562b0a549397afbfc78570079
Ahmed Mostafa <amostafa806@gmail.com>
2023-09-01 23:44:55 +0000

    Adding Docker-MySQL Settings

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="">
     <meta name="keywords" content="MediaCenter, Template, eCommerce">
     <meta name="robots" content="all">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Ecommerce Shop</title>
 
     <!-- Bootstrap Core CSS -->
