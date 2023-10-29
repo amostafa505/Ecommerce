183d2a849faa8a30f47d6e7679c9bf1d02f800dc
amostafa505 <57479974+amostafa505@users.noreply.github.com>
2023-10-29 22:50:42 +0200

    Finish CheckOut & Add Stripe Payment

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CheckoutController extends Controller
 {
     public function checkoutView(){
+        if(Auth::user()){
+            if(Cart::content()>1){
+
+            }else{
+                return redirect('/')->with('error' , 'You have to add least one item to view this page');
+            }
+        }else{
+            return redirect('login')->with('error' , 'You have to Login First');
+        }
         return view('frontend.checkout.CheckoutView');
     }//end function
 }
